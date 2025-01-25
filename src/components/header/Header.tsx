93c4c4a17c1f536b3526bb8b3c0ca3b4e17dff60
NicolasUrban2 <nclsurban@gmail.com>
2025-01-25 14:00:16 +0100

    Fix eslint errors

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import clsx from "clsx";
-import Link from "next/link";
 import Button from "../button/NavButton";
 
 export type HeaderProps = {
+    className?: string;
 };
 
 export default function Header(props: HeaderProps) {
+    const { className } = props;
 
     return (
-        <header className="flex justify-between items-center">
+        <header className={clsx(
+            "flex justify-between items-center",
+            className,
+        )}>
             <Button href="/">Home</Button>
             <Button href="/login">Login</Button>
         </header>
