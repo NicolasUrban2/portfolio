4e5381f86370b36b85673f4ccdf8e842a80332ad
NicolasUrban2 <nclsurban@gmail.com>
2025-01-26 17:19:10 +0100

    Redirect anonymous user to login when trying to go to protected route.

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,3 +1,4 @@
+import { authorizations } from '@/security/authorizations';
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
@@ -37,12 +38,12 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
+  const isProtectedRoute = authorizations.some((route) => request.nextUrl.pathname.startsWith(route));
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
+    isProtectedRoute
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
     return NextResponse.redirect(url)
---
+++ src/security/authorizations.ts
@@ -0,0 +1,3 @@
+export const authorizations: string[] = [
+    'dashboard',
+]
