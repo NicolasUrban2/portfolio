da904109a96c462a72591fa6ce345476008213e0
NicolasUrban2 <nclsurban@gmail.com>
2025-02-03 17:37:54 +0100

    Refactor PortfolioContents component to use Supabase client for data fetching; remove PortfolioContentsResource

--- src/components/dashboard/PortfolioContents.tsx
+++ src/components/dashboard/PortfolioContents.tsx
@@ -1,6 +1,6 @@
-import { PortfolioContentsResource } from "@/lib/supabase/resources/PortfolioContentsResource";
 import clsx from "clsx";
 import { PortfolioContentsEdit } from "../card/PortfolioContentEdit";
+import { createClient } from "@/lib/supabase/server";
 
 export type PortfolioContentsProps = {
     className?: string,
@@ -8,16 +8,19 @@ export type PortfolioContentsProps = {
 
 export async function PortfolioContents(props: PortfolioContentsProps) {
     const { className } = props;
-    const { data, error } = await PortfolioContentsResource.getList();
+
+    const supabase = await createClient();
+    const { data, error } = await supabase.from('portfolio_contents').select('*');
 
     return (
         <div className={clsx(
             '',
             className
         )}>
-            {data !== null ? data.map((portfolioContent, index) => <div>
-                <PortfolioContentsEdit portfolioContent={portfolioContent} />
-            </div> ) : error}
+            {data !== null ? data.map((portfolioContent, index) => 
+                <PortfolioContentsEdit key={portfolioContent.id} portfolioContent={portfolioContent} /> 
+            ) :
+                <div>Loading...</div>}
         </div>
     );
 }
